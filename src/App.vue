6e78e482d38f4d56412b08e59ebb205df25b1649
r.mavericks <r.mavericks@gmail.com>
2025-11-12 14:01:55 +0800

    all

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Navbar from './components/Navbar.vue'
+import About from './components/About.vue'
+import Projects from './components/Projects.vue'
+import Tools from './components/Tools.vue'
+import Contact from './components/Contact.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <Navbar />
+  <div class="container-fluid">
+  	<About />
+  	<Projects />
+  	<Tools />
+  	<Contact />
+  </div>
+  <Footer />
+</template>
+
+<style scoped>
+
+</style>
